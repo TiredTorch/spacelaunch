fdc33b5aebccb9c496b50116d86fa83ebab9e32b
Tr.Torch <vitochke337@gmail.com>
2022-04-22 11:51:59 +0300

    Added responsability to footer and main section

--- libs/spacelaunch/ui-shared/src/lib/content/content-section/content-section.tsx
+++ libs/spacelaunch/ui-shared/src/lib/content/content-section/content-section.tsx
@@ -1,25 +1,37 @@
-import { Stack, styled } from "@mui/material";
+import { Stack, styled } from '@mui/material';
 
-/* eslint-disable-next-line */
 export interface ContentSectionProps {
   children: React.ReactNode
 }
 
 const StyledStack = styled(Stack)(({ theme }) => ({
-  position: 'relative',
-  top: '-120px',
-  width: '75vw',
-  margin: 'auto',
-  padding: '4%',
-  backgroundColor: theme.palette.primary.light
-}))
+	position: 'relative',
+	top: '-120px',
+	width: '75vw',
+	margin: 'auto',
+	padding: '4%',
+	backgroundColor: theme.palette.primary.light,
+
+	[theme.breakpoints.down('lg')] : {
+		padding: '3%',
+		width: '80vw'
+	},
+	[theme.breakpoints.down('md')] : {
+		padding: '2%',
+		width: '90vw'
+	},
+	[theme.breakpoints.down('sm')] : {
+		padding: '2%',
+		width: '100vw'
+	},
+}));
 
 export function ContentSection(props: ContentSectionProps) {
-  return (
-    <StyledStack>
-      {props.children}
-    </StyledStack>
-  );
+	return (
+		<StyledStack>
+			{props.children}
+		</StyledStack>
+	);
 }
 
 export default ContentSection;
--- libs/spacelaunch/ui-shared/src/lib/footer/footer.tsx
+++ libs/spacelaunch/ui-shared/src/lib/footer/footer.tsx
@@ -1,33 +1,32 @@
-import { Box, styled, Typography } from "@mui/material";
-import Logo from "../common/logo/logo";
+import { Box, styled, Typography } from '@mui/material';
+import Logo from '../common/logo/logo';
 
-/* eslint-disable-next-line */
-export interface FooterProps {}
 
 const StyledBox = styled(Box)(({ theme }) => ({
-  width: '100vw',
-  height: '215px',
+	width: '100vw',
+	height: '5vh',
 
-  display: 'flex',
-  justifyContent: 'space-around',
-  alignItems: 'center'
-}))
+	display: 'flex',
+	justifyContent: 'space-around',
+	alignItems: 'center',
+	marginBottom: '5vh'
+}));
 const StyledCopyrights = styled(Typography)(({ theme }) => ({
-  color: theme.palette.text.secondary
-}))
+	color: theme.palette.text.secondary
+}));
 
-export function Footer(props: FooterProps) {
-  return (
-    <StyledBox>
-      <Logo
-        logoSize='sm'
-        src="https://www.pngmart.com/files/16/official-Google-Logo-PNG-Image.png"
-      />
-      <StyledCopyrights variant="h5">
+export function Footer() {
+	return (
+		<StyledBox>
+			<Logo
+				logoSize='sm'
+				src="https://www.pngmart.com/files/16/official-Google-Logo-PNG-Image.png"
+			/>
+			<StyledCopyrights variant="h5">
         © 2021 Copyright
-      </StyledCopyrights>
-    </StyledBox>
-  );
+			</StyledCopyrights>
+		</StyledBox>
+	);
 }
 
 export default Footer;
