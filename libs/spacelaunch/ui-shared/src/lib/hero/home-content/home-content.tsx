9fb23b3a1feb2e8b762b9e97cf050fed01225a33
Tr.Torch <vitochke337@gmail.com>
2022-04-22 11:17:13 +0300

    Hotfix hero home adaprivity

--- libs/spacelaunch/ui-shared/src/lib/hero/home-content/home-content.tsx
+++ libs/spacelaunch/ui-shared/src/lib/hero/home-content/home-content.tsx
@@ -1,13 +1,23 @@
 import { ListItem, Stack, styled, Typography } from '@mui/material';
 import Button from '../../common/button/button';
 
-const StyledStack = styled(Stack)({
-
-});
+const StyledStack = styled(Stack)(({ theme }) => ({
+	position: 'absolute',
+	left: '15vw',
+	[theme.breakpoints.down('lg')] : {
+		left: '10vw',
+	},
+	[theme.breakpoints.down('md')] : {
+		left: '10vw',
+	},
+	[theme.breakpoints.down('sm')] : {
+		left: '6vw',
+	},
+}));
 
 export function HomeContent() {
 	return (
-		<StyledStack spacing='30px'>
+		<StyledStack spacing={'20px'}>
 			<ListItem>
 				<Typography variant="h1">
           Upcoming <br/>Spaceflight Launches
